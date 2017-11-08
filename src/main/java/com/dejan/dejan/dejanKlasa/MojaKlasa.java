5b0010b807643a8ce5739aebdc63ba10532ba00a
re5pect123 <re5pectus123@gmail.com>
2017-11-08 23:19:55 +0100

    vracena verzija na prvu

--- src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
+++ src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
@@ -5,6 +5,8 @@ public class MojaKlasa {
 
     public String dejan(){
         System.out.println("OVO JE VERZIJA 1 KOJA RADI");
+
+        System.out.println("Ovo je verzija vracana broj 1");
         return "Ovo radi";
     };
 }
