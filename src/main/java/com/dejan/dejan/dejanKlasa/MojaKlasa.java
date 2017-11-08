df0df7fde9aa16420aff14b452f68a414b9d4b89
re5pect123 <re5pectus123@gmail.com>
2017-11-08 23:16:30 +0100

    vracena verzija 3

--- src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
+++ src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
@@ -4,6 +4,12 @@ public class MojaKlasa {
 
     public String dejan(){
 
+<<<<<<< HEAD
+=======
+        System.out.println("Ovo je verzija 2 koja radi");
+
+        System.out.println("Ovo je verzija 3 koja radi");
+>>>>>>> 3685f2a... verzija 3 buraz
         return "Ovo radi";
     };
 
