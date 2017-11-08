97c92fcf7c0c5874836dbe4676cb9a348c16effe
re5pect123 <re5pectus123@gmail.com>
2017-11-08 23:23:09 +0100

    ubaci klasu

--- src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
+++ src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
@@ -5,6 +5,7 @@ public class MojaKlasa {
 
     public String dejan(){
         System.out.println("OVO JE VERZIJA 1 KOJA RADI");
+        System.out.println("VRACENA PRVA VERZIJA");
         return "Ovo radi";
     };
 }
