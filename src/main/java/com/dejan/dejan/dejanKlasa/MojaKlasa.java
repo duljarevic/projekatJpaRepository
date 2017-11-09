d2d2fcbd8d789f03851771d96b0d1b4c89554ad6
DESKTOP-A45I146\dejan <duljarevic@gmail.com>
2017-11-09 01:39:08 -0800

    ovo je prava verzija

--- src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
+++ src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
@@ -3,20 +3,7 @@ package com.dejan.dejan.dejanKlasa;
 public class MojaKlasa {
 
 
-    public String dejan(){
-        System.out.println("OVO JE VERZIJA 1 KOJA RADI");
-<<<<<<< HEAD
-        System.out.println("VRACENA PRVA VERZIJA");
-=======
+    public void dejan(){
 
-<<<<<<< HEAD
-        System.out.println("Ovo je verzija vracana broj 1");
-=======
-        System.out.println("Ovo je verzija 2 koja radi");
-
-        System.out.println("Ovo je verzija 3 koja radi");
->>>>>>> 3685f2a1e0cebb806017dc0ed9206ea699abf264
->>>>>>> a876ba4694f1f30737ef83a5b39ec25d7b6dfe5a
-        return "Ovo radi";
-    };
+    }
 }
