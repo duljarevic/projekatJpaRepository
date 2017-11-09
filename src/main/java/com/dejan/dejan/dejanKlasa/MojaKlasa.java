a61f55d4cbf91a0ac5b695c4af0d197de07d117e
DESKTOP-A45I146\dejan <duljarevic@gmail.com>
2017-11-09 12:38:54 -0800

    2222

--- src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
+++ src/main/java/com/dejan/dejan/dejanKlasa/MojaKlasa.java
@@ -4,10 +4,6 @@ public class MojaKlasa {
 
     public String dejan(){
 
-        System.out.println("Ovo je verzija 2 koja radi");
-
-        System.out.println("Ovo je verzija 3 koja radi");
-
         return "Ovo radi";
     };
 
